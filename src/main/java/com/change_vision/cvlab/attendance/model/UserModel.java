3771c2cfef0cf51558a142d54571ba7cd49125cd
shiiyaaoyama <shiiya.aoyama@change-vision.com>
2021-01-16 17:01:45 +0900

    UserModelを実装した。

--- src/main/java/com/change_vision/cvlab/attendance/model/UserModel.java
+++ src/main/java/com/change_vision/cvlab/attendance/model/UserModel.java
@@ -1,4 +1,17 @@
 package com.change_vision.cvlab.attendance.model;
 
 public class UserModel {
+    private String name;
+    private Role role;
+    public UserModel(String name, Role role){
+        this.name = name;
+        this.role = role;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public boolean isAdmin(){
+        return this.role == Role.ADMIN;
+    }
 }
